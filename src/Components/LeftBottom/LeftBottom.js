7bd86774ac54dcc37dfc59725749e98c95a1099d
mutasem <motasemimad10@gmail.com>
2022-04-16 05:33:36 +0300

    setup resturent screen

--- src/Components/LeftBottom/LeftBottom.js
+++ src/Components/LeftBottom/LeftBottom.js
@@ -1,38 +1,28 @@
-
-import React from 'react'
-import { TouchableOpacity, View } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
-import { ScaledSheet } from 'react-native-size-matters'
-import COLORS from '../../Assistant/Color'
-
+import React from 'react';
+import {TouchableOpacity, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {ScaledSheet} from 'react-native-size-matters';
+import COLORS from '../../Assistant/Color';
 
 export function LeftBottom(props) {
+  const {OnClickButtom} = props;
 
-    const { OnClickButtom } = props
-
-
-
-    return <TouchableOpacity onPress={OnClickButtom} >
-
-        <Icon name="arrow-back-outline" style={Styles.IconsLeft} />
-
+  return (
+    <TouchableOpacity onPress={OnClickButtom}>
+      <Icon name="arrow-back-outline" style={Styles.IconsLeft} />
     </TouchableOpacity>
-
+  );
 }
 
-
-
-
 const Styles = ScaledSheet.create({
-    IconsLeft: {
-        width: '40@s',
-        height: '40@s',
-        borderRadius: '50@s',
-        backgroundColor: COLORS.firstRed,
-        color: '#eee',
-        fontSize: '30@s',
-        lineHeight: '40@s',
-        textAlign: 'center'
-    }
-
-})
+  IconsLeft: {
+    width: '40@s',
+    height: '40@s',
+    borderRadius: '50@s',
+    backgroundColor: COLORS.firstRed,
+    color: '#eee',
+    fontSize: '30@s',
+    lineHeight: '40@s',
+    textAlign: 'center',
+  },
+});
--- src/Pages/RestaurantScreen/RestaurantScreen.js
+++ src/Pages/RestaurantScreen/RestaurantScreen.js
@@ -1,17 +1,42 @@
+import React, { useState } from "react";
+import { View,Text, TouchableOpacity, Image,ScrollView } from "react-native";
+import Styles from './Styles'
+import Icon from "react-native-vector-icons/Ionicons";
+import { SliderBox } from "react-native-image-slider-box";
+import ButtonScreen from "../../Components/ButtonScreen/ButtonScreen";
 
-
-import React from "react";
-import { View,Text } from "react-native";
 
 
 export default function RestaurantScreen() {
+    const [images, setImages] = useState([
+        "https://source.unsplash.com/1024x768/?nature",
+        "https://source.unsplash.com/1024x768/?water",
+        "https://source.unsplash.com/1024x768/?girl",
+        "https://source.unsplash.com/1024x768/?tree",
+    ])
 
-
-
-
-
-
-    return <View>
-        <Text>Restaurant Screen .....</Text>
+    return <ScrollView style={Styles.containerChildren}>
+       <View style={Styles.containerLoaction}>
+       <Icon name="location-outline" style={Styles.IconsLeft} />
+       <Text style={Styles.textCity}>
+        uppsala
+       </Text>
+       <Icon name="chevron-down-outline" style={Styles.iconDown} />
     </View>
+    <View style={Styles.containerSlider}>
+    <SliderBox images={images} style={Styles.imagesSlider} />
+    </View>
+
+    <View style={Styles.storesDelivering}>
+        <Text style={Styles.titleHeader}>Stores delivering to you</Text>
+        <TouchableOpacity>
+            <Text style={Styles.seeAllButton}>See All</Text>
+        </TouchableOpacity>
+    </View>
+
+    <View style={Styles.cardsContainer}>
+            <Image source={{ uri: 'https://source.unsplash.com/1024x768/?tree' }} style={Styles.stretch} />
+    </View>
+
+    </ScrollView>
 }
