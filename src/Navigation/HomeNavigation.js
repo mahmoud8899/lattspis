5dec3e908268e586079f633fac855c93038f8941
mutasem <motasemimad10@gmail.com>
2022-04-18 12:55:52 +0300

    edit in home screen navigation

--- src/Navigation/HomeNavigation.js
+++ src/Navigation/HomeNavigation.js
@@ -1,32 +1,23 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import HomeScreen from "../Pages/HomeScreen/HomeScreen"
-import AllStoresDelivering from '../Pages/HomeScreen/AllStoresDelivering/AllStoresDelivering'
-import React from "react"
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from '../Pages/HomeScreen/HomeScreen'
+import SeeAllScreen from '../Pages/HomeScreen/SeeAllScreen/SeeAllScreen'
+import React from 'react'
 
 const Stack = createNativeStackNavigator()
 
 export default function HomeNavigation() {
-
-    return <Stack.Navigator>
-        <Stack.Screen
+   return (
+      <Stack.Navigator>
+         <Stack.Screen
             name="Home"
             component={HomeScreen}
             options={{ headerShown: false }}
-        />
-        <Stack.Screen
+         />
+         <Stack.Screen
             name="SeeAll"
-            component={AllStoresDelivering}
+            component={SeeAllScreen}
             options={{ headerShown: false }}
-        />
-
-
-    </Stack.Navigator>
-
+         />
+      </Stack.Navigator>
+   )
 }
-
-
-
-
-
-
