ad29b710f96ba0b59731ea01e09da898207fb7ae
mutasem <motasemimad10@gmail.com>
2022-04-21 15:14:17 +0300

    clear code

--- src/Components/HeaderLocation/Styles.js
+++ src/Components/HeaderLocation/Styles.js
@@ -2,62 +2,38 @@ import { ScaledSheet, scale } from 'react-native-size-matters'
 import COLOR from '../../Assistant/Color'
 
 const Styles = ScaledSheet.create({
-   margintop:{
-
-      marginBottom : '60@s',
-
-
+   margintop: {
+      marginBottom: '60@s',
    },
    stad: {
       // backgroundColor: 'red',
       marginBottom: '10@s',
       justifyContent: 'center',
       // alignItems: 'center',
-      height : '50@s'
+      height: '50@s',
    },
    city: {},
    FirstContainer: {
-<<<<<<< HEAD
-=======
-
->>>>>>> 4a0b51d0ecebfe107b6166bd617bb5a9b512f2dd
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#1211110d',
-<<<<<<< HEAD
+
       borderRadius: '20@s',
    },
    LastContainer: {
-      flex: 1,
+      flex: 3,
       borderRadius: '20@s',
       padding: '10@s',
       backgroundColor: COLOR.white,
    },
    route: {
-=======
-      borderRadius: '20@s'
-
-   },
-   LastContainer: {
-
-      flex: 3,
-      borderRadius: '20@s',
-      padding: '10@s',
-      backgroundColor: COLOR.white
-
-
-   },
-   route: {
-
->>>>>>> 4a0b51d0ecebfe107b6166bd617bb5a9b512f2dd
       width: '40@s',
       height: '40@s',
       borderRadius: 50,
       backgroundColor: COLOR.firstRed,
       justifyContent: 'center',
       alignItems: 'center',
-<<<<<<< HEAD
       marginLeft: 'auto',
    },
    left: {
@@ -66,19 +42,6 @@ const Styles = ScaledSheet.create({
    ICON: {
       color: COLOR.white,
       fontSize: '30@s',
-=======
-      marginLeft: 'auto'
-
-   },
-   left: {
-      marginLeft: '0@s',
-
-   },
-   ICON: {
-      color: COLOR.white,
-      fontSize: '30@s'
-
->>>>>>> 4a0b51d0ecebfe107b6166bd617bb5a9b512f2dd
    },
    containerLocation: {
       flexDirection: 'row',
@@ -105,7 +68,6 @@ const Styles = ScaledSheet.create({
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'flex-start',
-<<<<<<< HEAD
       marginTop: '20@s',
    },
    flexLocationText: {
@@ -113,21 +75,10 @@ const Styles = ScaledSheet.create({
    },
    MARGINB: {
       marginBottom: '3@s',
-=======
-      marginTop: '20@s'
-   },
-   flexLocationText: {
-      marginLeft: '10@s'
-   },
-   MARGINB: {
-      marginBottom: '3@s'
->>>>>>> 4a0b51d0ecebfe107b6166bd617bb5a9b512f2dd
    },
    Top: {
       width: '100%',
       height: '1@s',
-      backgroundColor: COLOR.firstRed,
-<<<<<<< HEAD
       marginTop: '20@s',
    },
    notColor: {
@@ -137,18 +88,6 @@ const Styles = ScaledSheet.create({
       color: COLOR.light,
       fontSize: '30@s',
    },
-=======
-      marginTop: '20@s'
-   },
-   notColor: {
-      backgroundColor: COLOR.white,
-   },
-   BALCK: {
-      color: COLOR.light,
-      fontSize: '30@s'
-   }
-
->>>>>>> 4a0b51d0ecebfe107b6166bd617bb5a9b512f2dd
 })
 
 export default Styles
