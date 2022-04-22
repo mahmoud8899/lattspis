ec8660437bdc380a67c830305f37c74f0a97e480
mutasem <motasemimad10@gmail.com>
2022-04-22 18:42:39 +0300

    add language to location screen

---
+++ src/Language/Location.js
@@ -0,0 +1,21 @@
+const Lang = {
+   dontKnow: {
+      EN: "We don't know where you are",
+      SW: 'Vi vet inte var du är',
+   },
+   shareLocation: {
+      EN:
+         'Please share your delivery location or enter the address manually below.',
+      SW: 'Vänligen dela din leveransplats eller ange adressen manuellt nedan.',
+   },
+   shareLocationBtn: {
+      EN: 'Share Location',
+      SW: 'Dela plats',
+   },
+   enterAddress: {
+      EN: 'Enter address manually',
+      SW: 'Ange adressen manuellt',
+   },
+}
+
+export default Lang
--- src/Pages/LocationScreen/LocationScreen.js
+++ src/Pages/LocationScreen/LocationScreen.js
@@ -1,46 +1,51 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './Styles'
 import FontsDefault from '../../Assistant/FontDefault'
 import HeaderLocation from '../../Components/HeaderLocation/HeaderLocation'
 import ButtonScreen from '../../Components/ButtonScreen/ButtonScreen'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { ChoseLanguageDatilas } from '../../Components/UseContext/ChoseLanguage'
+import LocationLang from '../../Language/Location'
+
 export default function LocationScreen() {
+   const { Language } = useContext(ChoseLanguageDatilas)
 
+   return (
+      <View style={FontsDefault.containerChildren}>
+         <HeaderLocation />
 
+         <KeyboardAwareScrollView extraHeight={120} enableOnAndroid>
+            <View style={[Styles.marginTop, FontsDefault.containerImage]}>
+               <Image
+                  style={FontsDefault.containerImageChildren}
+                  source={require('../../../data/myimage/location.png')}
+               />
+            </View>
 
+            <View style={[Styles.center, FontsDefault.marginTopAndBottom]}>
+               <Text style={[FontsDefault.TitleFont, FontsDefault.FontColor]}>
+                  {LocationLang.dontKnow[Language]}
+               </Text>
+            </View>
 
-    return <View style={FontsDefault.containerChildren}>
-        <HeaderLocation />
+            <View style={[Styles.center, FontsDefault.marginTopAndBottom]}>
+               <Text style={FontsDefault.fontDescription}>
+                  {LocationLang.shareLocation[Language]}
+               </Text>
+            </View>
 
-        <KeyboardAwareScrollView extraHeight={120} enableOnAndroid>
-        <View style={[Styles.marginTop, FontsDefault.containerImage]} >
-            <Image
-                style={FontsDefault.containerImageChildren}
-                source={require('../../../data/myimage/location.png')}
-            />
-        </View>
-
-        <View style={[Styles.center, FontsDefault.marginTopAndBottom]}>
-            <Text style={[FontsDefault.TitleFont, FontsDefault.FontColor]}>We don't know where you are</Text>
-        </View>
-
-        <View style={[Styles.center, FontsDefault.marginTopAndBottom]}>
-            <Text style={FontsDefault.fontDescription}>Please share your delivery location or enter the
-                address manually below.</Text>
-        </View>
-
-        <View style={FontsDefault.marginTopAndBottom}>
-            <ButtonScreen Titel='Share location' />
-        </View>
-        <View style={FontsDefault.marginTopAndBottom}>
-            <ButtonScreen
-                Titel='Enter address manually'
-                styleTouch={Styles.ContainerButtom}
-                StyleTitel={[Styles.color]}
-
-            />
-        </View>
-        </KeyboardAwareScrollView>
-    </View>
+            <View style={FontsDefault.marginTopAndBottom}>
+               <ButtonScreen Titel={LocationLang.shareLocationBtn[Language]} />
+            </View>
+            <View style={FontsDefault.marginTopAndBottom}>
+               <ButtonScreen
+                  Titel={LocationLang.enterAddress[Language]}
+                  styleTouch={Styles.ContainerButtom}
+                  StyleTitel={[Styles.color]}
+               />
+            </View>
+         </KeyboardAwareScrollView>
+      </View>
+   )
 }
