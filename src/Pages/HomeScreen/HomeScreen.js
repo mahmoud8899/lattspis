7c3813dc5a02c5344038851bd95b290b979deb3e
mutasem <motasemimad10@gmail.com>
2022-04-24 18:52:09 +0300

    add maps to app

--- src/Pages/HomeScreen/HomeScreen.js
+++ src/Pages/HomeScreen/HomeScreen.js
@@ -8,6 +8,7 @@ import CartItems from '../../Components/CartItems/CartItems'
 import FontsDefault from '../../Assistant/FontDefault'
 import HomeLang from '../../Language/Home'
 import { ChoseLanguageDatilas } from '../../Components/UseContext/ChoseLanguage'
+import MapView from 'react-native-maps'
 
 export default function HomeScreen({ navigation }) {
    const { Language } = useContext(ChoseLanguageDatilas)
@@ -57,6 +58,24 @@ export default function HomeScreen({ navigation }) {
                forCategory={true}
                navigation={navigation}
             />
+
+            <View
+               style={{
+                  width: 500,
+                  height: 500,
+                  flex: 1,
+               }}
+            >
+               <MapView
+                  style={{ width: '100%', height: '100%' }}
+                  initialRegion={{
+                     latitude: 37.78825,
+                     longitude: -122.4324,
+                     latitudeDelta: 0.0922,
+                     longitudeDelta: 0.0421,
+                  }}
+               />
+            </View>
          </ScrollView>
       </View>
    )
